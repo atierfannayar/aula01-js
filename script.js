49b8a47c5614394a280b091525c98c86c80a86dd
atierfannayar <rayanna.fsilva@senacsp.edu.br>
2024-08-21 21:35:21 -0300

    feat: Correção da média 2 alunos

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
-/* 
-Faça um algoritmo onde o usuário informe o nome e 
-4 notas de dois alunos e mostre as 
-médias desses dois alunos.
-*/ 
+var nome1, nome2 = "";
+var nota1, nota2, nota3, nota4,
+    nota5, nota6, nota7, nota8 = 0;
 
-var nome = window.prompt("Informe o nome do aluno:");
-//Guardar a primeira nota
-var nota1 = window.prompt("Digite a nota 1:");
-//converte a nota1 em texto para decimal
-nota1 = parseFloat(nota1);
-//Guardar a segunda nota
-var nota2 = window.prompt("Digite a nota 2:");
-//converte a nota2 em texto para decimal
-nota2 = parseFloat(nota2);
-//Guardar a terceira nota
-var nota3 = window.prompt("Digite a nota 3:");
-//converte a nota3 em texto para decimal
-nota3 = parseFloat(nota3);
-//Guardar a quarta nota
-var nota4 = window.prompt("Digite a nota 4:");
-//converte a nota4 em texto para decimal
-nota4 = parseFloat(nota4);
-//calculo a média das quatro notas
-var media = (nota1 + nota2 + nota3 + nota4)/4
-//Gero
+nome1 = prompt("Digite o nome do aluno 1:");
+nota1 = parseFloat(prompt("Digite a nota 1 do aluno 1: "));
+nota2 = parseFloat(prompt("Digite a nota 2 do aluno 1: "));
+nota3 = parseFloat(prompt("Digite a nota 3 do aluno 1: "));
+nota4 = parseFloat(prompt("Digite a nota 4 do aluno 1: "));
+
+nome2 = prompt("Digite o nome do aluno 2:");
+nota5 = parseFloat(prompt("Digite a nota 1 do aluno 2: "));
+nota6 = parseFloat(prompt("Digite a nota 2 do aluno 2: "));
+nota7 = parseFloat(prompt("Digite a nota 3 do aluno 2: "));
+nota8 = parseFloat(prompt("Digite a nota 4 do aluno 2: "));
+
+var media1, media2 = 0;
+media1 = (nota1 + nota2 + nota3 + nota4) / 4;
+media2 = (nota5 + nota6 + nota7 + nota8) / 4;
+
+var mensagem = `
+${nome1}, sua media é ${media1}, parabéns! 
+${nome2}, sua media é ${media2}, parabéns!`;
+
+alert(mensagem);
